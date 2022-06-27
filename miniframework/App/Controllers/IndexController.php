799566ab926a1ec9418d65dfa7044a6f805a313a
Vitor Hugo Freire Franco Gonçales <vitorhugofreire@hotmail.com.br>
2022-06-26 21:27:51 -0300

    first commit

---
+++ miniframework/App/Controllers/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+    namespace App\Controllers;
+
+    use MF\Controller\Action;
+
+    class IndexController extends Action {
+
+        public function index() {
+
+            $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+            $this->render('index', 'layout2');
+        }
+
+        public function sobreNos() {
+
+            $this->view->dados = array('Notebook', 'Smartphone');
+            $this->render('sobreNos',  'layout1');
+        }
+
+    }
+
+?>
